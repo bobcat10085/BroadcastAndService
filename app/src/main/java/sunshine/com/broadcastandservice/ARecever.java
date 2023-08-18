c45b955ebbc830f3c079b018e235ef73bdc1cb7a
bobcat10085 <142510967+bobcat10085@users.noreply.github.com>
2023-08-19 03:08:06 +0800

    Update ARecever.java

--- app/src/main/java/sunshine/com/broadcastandservice/ARecever.java
+++ app/src/main/java/sunshine/com/broadcastandservice/ARecever.java
@@ -1,22 +1,22 @@
-包sunshine.com.broadcastandservice；
+package sunshine.com.broadcastandservice;
 
-导入android.content.BroadcastReceiver;
-导入android.content.Context；
-导入android.content.Intent;
-导入android.util.Log；
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 
 /**
-* 由 a 于 2016/6/19 创建。
-*/
-公共 类 MyReceiver 扩展了BroadcastReceiver {
+ * Created by a on 2016/6/19.
+ */
+public class MyReceiver extends BroadcastReceiver {
 	
-	    @覆盖
-	    公共 无效 onReceive （上下文上下文，意图意图） {
-	// 获取外拨电话的电话号码
-	// （从当前的广播中抽取结果数据，一般为null，但该事件中获得了外拨号码）
-	        String电话号码= getResultData ( ) ;
-	        日志。e ( "qqq" , "打电话：" +电话号码) ;
-	// 下面是设置外拨电话的号码，你会发现无论你打给谁，都打给了10010
-	        setResultData ( "10010" ) ;
+	    @Override
+	    public void onReceive(Context context, Intent intent) {
+	//        获取外拨电话的电话号码
+	//       （从当前的广播中抽取结果数据，一般为null，但这个事件中获得了外拨号码）
+	        String phoneNumber = getResultData();
+	        Log.e("qqq","打电话:"+phoneNumber);
+	//        下面是设置外拨电话的号码，你会发现无论你打给谁，都打给了10010
+	        setResultData("10010");
 	    }
 	}
